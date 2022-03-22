77575e958280be9c13b86422833c236c7097c9e3
Nirmal Ghimire <ghimire.nirmal@yahoo.com>
2022-03-22 11:40:17 -0500

    added new project: H1B study

--- plot2.R
+++ plot2.R
@@ -8,6 +8,9 @@ SCC <- readRDS("Source_Classification_Code.rds")
 #answering this question.
 subsetNEI<-NEI[NEI$fips=="24510",]
 emissionByYear<-aggregate(Emissions~year,subsetNEI,sum)
+emissionByYear
+mean_emmissionByYear <- aggregate(Emissions~year,subsetNEI,mean)
+mean_emmissionByYear
 
 #Creating plot2.png
 png('plot2.png')
@@ -16,4 +19,14 @@ barplot(height=emissionByYear$Emissions, names.arg = emissionByYear$year,
         xlab="Years",ylab=expression('Total PM2.5 Emission'),ylim=c(0,4000),
         main=expression ('Total PM2.5 Emissions in Baltimore City by Years'),
         col=diffcolor)
+dev.off()
+
+# Creating plot7.png using mean_emmissionByYear
+png("plot7.png",width=640,height=480)
+img<-ggplot(mean_emmissionByYear,aes(year,Emissions,color=year))
+img<-img+geom_line()+
+  xlab("Year")+
+  ylab(expression("Mean PM2.5 Emissions"))+
+  ggtitle("Average Emissions in Baltimore City,Maryland from 1999 to 2008")
+print(img)
 dev.off()
--- plot3.R
+++ plot3.R
@@ -11,7 +11,11 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 #Subsetting Baltimore City
 subsetNEI<-NEI[NEI$fips=="24510",]
+subsetNEI
 TotalByYearType<-aggregate(Emissions~year+type,subsetNEI,sum)
+TotalByYearType
+MeanByYearType <- aggregate(Emissions~year+type, subsetNEI, mean)
+MeanByYearType
 
 #Creating plot3.png
 png("plot3.png",width=640,height=480)
@@ -20,5 +24,14 @@ img<-img+geom_line()+
   xlab("Year")+
   ylab(expression("Total PM2.5 Emissions"))+
   ggtitle("Total Emissions in Baltimore City,Maryland from 1999 to 2008")
+print(img)
+
+#Creating plot8.png
+png("plot8.png",width=640,height=480)
+img<-ggplot(MeanByYearType,aes(year,Emissions,color=type))
+img<-img+geom_line()+
+  xlab("Year")+
+  ylab(expression("Mean PM2.5 Emissions"))+
+  ggtitle("Average Emissions in Baltimore City,Maryland from 1999 to 2008 by Type")
 print(img)
 dev.off()
